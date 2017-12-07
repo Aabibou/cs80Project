a9603c26f79916240568a2643ebe05a52528869c
Abibou <abib94alouga@gmail.com>
2017-12-07 03:30:28 -0800

    Start button works. Thanks!

---
+++ main_js.js
@@ -0,0 +1,9 @@
+function welcomeText(){
+    //English, Spannish, French.
+    var greet= ["Welcome, click  on Games to play your favorite game.",
+                "Bienvenido, haz clic en Games para jugar a tu juego favorito."
+               "Bienvenue, appuyer sur Games pour jouer ton jeu préféré",
+                ""
+               ]
+    return greet[0];
+}
